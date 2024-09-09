97971475cc09308a32f31ac6ff41962887b25a3e
alimar440 <ngomali440@gmail.com>
2024-09-09 21:04:10 +0000

    push

--- epreuve/Examen_1er_session_21-22/main.cpp
+++ epreuve/Examen_1er_session_21-22/main.cpp
@@ -11,7 +11,11 @@ int main() {
  
     Magasin magasin(5); 
 
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 2864bc249ae0d3dc143546a1ef58a590d362b382
     char design1[] = "ordinateur";
     char design2[] = "telephone";
     char design3[] = "Tablette";
@@ -21,7 +25,11 @@ int main() {
     Article* article1 = new ArticleImporter(32123, design1, 100000, 15000); 
     Article* article2 = new ArticleImporter(2334, design4, 70000, 20000); 
     
+<<<<<<< HEAD
     
+=======
+   
+>>>>>>> 2864bc249ae0d3dc143546a1ef58a590d362b382
     Article* article3 = new ArticleLocal(12303, design2, 50000, 5000); 
     Article* article4 = new ArticleLocal(16404, design3, 20000, 30000); 
     Article* article5 = new ArticleLocal(12305, design5, 15000, 2000); 
